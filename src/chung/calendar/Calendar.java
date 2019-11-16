9f8439800af98a8439b9723328f53eceb644e962
임충환 <@DESKTOP-QSSQ0G0>
2019-11-16 15:32:32 +0900

    Fix encoding problem

--- src/chung/calendar/Calendar.java
+++ src/chung/calendar/Calendar.java
@@ -2,6 +2,7 @@ package chung.calendar;
 
 public class Calendar {
 	public static void main(String[] args) {
+		System.out.println("Calendar");
 		System.out.println("일   월   화   수   목   금   토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
