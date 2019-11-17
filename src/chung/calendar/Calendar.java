48b232844ee4250e1bc62723fcaffed2b147e2d4
임충환 <@DESKTOP-QSSQ0G0>
2019-11-17 18:30:00 +0900

    max Days of month modify

--- src/chung/calendar/Calendar.java
+++ src/chung/calendar/Calendar.java
@@ -1,6 +1,14 @@
 package chung.calendar;
+
 import java.util.Scanner;
+
 public class Calendar {
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int maxDaysOfmonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Calendar");
 		System.out.println("일   월   화   수   목   금   토");
@@ -10,53 +18,15 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		
-		//입력 받은 월의 최대 일수 출력
-		System.out.println("달을 입력하세요.");
-		Scanner scanner = new Scanner(System.in);
-		int month = scanner.nextInt();
-		
-		int [] calendar = new int[12];
-			if(month==1) {
-				System.out.println("1월은 31일까지 있습니다.");
-			}
-			else if(month==2) {
-				System.out.println("2월은 28일까지 있습니다.");
-			}
-			else if(month==3) {
-				System.out.println("3월은 31일까지 있습니다.");
-			}
-			else if(month==4) {
-				System.out.println("4월은 30일까지 있습니다.");
-			}
-			else if(month==5) {
-				System.out.println("5월은 31일까지 있습니다.");
-			}
-			else if(month==6) {
-				System.out.println("6월은 30일까지 있습니다.");
-			}
-			else if(month==7) {
-				System.out.println("7월은 31일까지 있습니다.");
-			}
-			else if(month==8) {
-				System.out.println("8월은 31일까지 있습니다.");
-			}
-			else if(month==9) {
-				System.out.println("9월은 30일까지 있습니다.");
-			}
-			else if(month==10) {
-				System.out.println("10월은 31일까지 있습니다.");
-			}
-			else if(month==11) {
-				System.out.println("11월은 30일까지 있습니다.");
-			}
-			else if(month==12) {
-				System.out.println("12월은 31일까지 있습니다.");
-			}
-		for(int i=1; i<calendar.length; i++) {
+	    //숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+			Scanner scanner = new Scanner(System.in);
+			Calendar cal = new Calendar();
+			System.out.println("달을 입력하세요");
+			int month = scanner.nextInt();
 			
-		}
-
-		scanner.close();
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.maxDaysOfmonth(month));
+		    scanner.close();
 	}
 
 }
+
